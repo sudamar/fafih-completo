6e8d5f2327b24781a95225e37fb04407765d3448
Sudamar <sudamar@gmail.com>
2025-09-16 21:21:51 -0300

    Adicionando extensao

--- src/pages/ExtensaoPage.jsx
+++ src/pages/ExtensaoPage.jsx
@@ -3,20 +3,21 @@ import { Link } from 'react-router-dom';
 
 const ExtensaoPage = () => {
   return (
-    <main>
-      <section className="page-section">
-        <div className="container">
-          <h1>Extensão Universitária</h1>
-          <p className="page-intro">A Curricularização da Extensão é um processo orgânico e dinâmico. Ocorre quando a Faculdade oferece importantes serviços à população, realizando a integração entre teoria e prática.</p>
+       <section className="page-section">
+      <div className="container">
+        <h1>Extensão Universitária</h1>
 
-          <ExtensaoTabs />
-
-          <div className="page-actions">
-            <Link to="/" className="btn-page-action btn-secondary">Voltar</Link>
-          </div>
+        <div className="conheca-intro">
+          <p>A Curricularização da Extensão é um processo orgânico e dinâmico. Ocorre quando a Faculdade oferece importantes serviços à população, realizando a integração entre teoria e prática.</p>
         </div>
-      </section>
-    </main>
+
+        <ExtensaoTabs />
+
+        <div className="page-actions">
+          <Link to="/" className="btn-page-action btn-secondary">Voltar</Link>
+        </div>
+      </div>
+    </section>
   );
 };
 
