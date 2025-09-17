9c02c6c6d49a6b3b5aa58363657b9b98eb1141c3
Sudamar <sudamar@gmail.com>
2025-09-17 18:42:12 -0300

    ajustes Footer e Index

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -20,16 +20,7 @@ const Footer = () => {
 
   return (
     <>
-      <section id="noticias">
-        <div className="container">
-          <h2>Notícias</h2>
-          <div className="noticias-container">
-            <a href="noticia-cursos.html" className="noticia-item">Cursos de Psicologia Junguiana; Arteterapia e Psicossomática</a>
-            <a href="noticia-carta-aberta.html" className="noticia-item">Carta aberta do coordenador dos cursos da FAFIH aos alunos e interessados</a>
-            <a href="noticia-duvidas-frequentes.html" className="noticia-item">Dúvidas Frequentes - Perguntas e Respostas - Informações a respeito dos Cursos</a>
-          </div>
-        </div>
-      </section>
+      
 
       <footer id="contato">
         <div className="footer-top-container">
--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -3,6 +3,7 @@ import About from '../components/index/About';
 import Atendimentos from '../components/index/Atendimentos';
 import Cursos from '../components/index/Cursos';
 import Depoimentos from '../components/index/Depoimentos';
+import Noticias from '../components/index/Noticias';
 
 const IndexPage = () => {
   return (
@@ -12,6 +13,7 @@ const IndexPage = () => {
       <Atendimentos />
       <Cursos />
       <Depoimentos />
+      <Noticias />
     </main>
   );
 };
